e150659c24ea5bab46063d41d59781a2e37e5d4e
Truls Johan Jacobsen-Lamøy <truls@Trulss-MBP.home>
2024-11-12 15:23:29 +0100

    feat: added the fetch urls to const

---
+++ src/js/api/constants.js
@@ -0,0 +1,20 @@
+export const API_KEY = "31ea6d50-4b21-49dd-8f59-63ae6776fc7a";
+
+export const API_BASE = "https://v2.api.noroff.dev/";
+
+export const API_AUTH = `${API_BASE}/auth`;
+
+export const API_AUTH_LOGIN = `${API_AUTH}/login`;
+
+export const API_AUTH_REGISTER = `${API_AUTH}/register`;
+
+export const API_AUTH_KEY = `${API_AUTH}/create-api-key`;
+
+/*
+const options = {
+    headers: {
+      'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoidHJ1bHMxMjMiLCJlbWFpbCI6InRydWxzMTIzQHN0dWQubm9yb2ZmLm5vIiwiaWF0IjoxNzMxNDIwODA3fQ.3QkCiSnf21QUuaT9lIzFda0dQQ23TQaSdYBwrjYkWns',
+      'X-Noroff-API-Key': '31ea6d50-4b21-49dd-8f59-63ae6776fc7a'
+    }
+  };
+  */
