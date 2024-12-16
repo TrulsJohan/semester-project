9fb72c4602726f8d5e0c7ea1402d4b140f7939d1
Truls Johan Jacobsen-Lamøy <truls@Trulss-MacBook-Pro.local>
2024-12-16 17:11:10 +0100

    test

--- vite.config.js
+++ vite.config.js
@@ -9,11 +9,11 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, "./index.html"),
-        login: resolve(__dirname, "./auth/login/login.html"),
-        register: resolve(__dirname, "./auth/register/register.html"),
-        create: resolve(__dirname, "./post/create/create.html"),
+        login: resolve(__dirname, "./auth/login/index.html"),
+        register: resolve(__dirname, "./auth/register/index.html"),
+        create: resolve(__dirname, "./post/create/index.html"),
         profile: resolve(__dirname, "./profile/index.html"),
-        post: resolve(__dirname, "./post/post/post.html"),
+        post: resolve(__dirname, "./post/post/index.html"),
       },
     },
   },
